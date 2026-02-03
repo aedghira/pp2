cb1a8f1a7b53cb42c2a77bd0fda0376c8ff27424
aedghira <ellegida514@gmail.com>
2026-02-03 18:05:15 +0500

    examples to lists are in progress

---
+++ practice2/lists.py
@@ -0,0 +1,175 @@
+#Example on W3schools
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#My own examples
+#1
+a = [1, 2, 3, 4]
+print(a)
+#2
+a = ['1', '2', '3', '4']
+print(a)
+#3
+pp_languages = ['python', 'c++', 'Java script', 'c#', 'Java', 'Kotlin']
+print(pp_languages)
+#4
+vegetables = ['potato', 'tomato', 'cucumber', 'carrot', 'onion']
+print(vegetables)
+#5
+a = [1, 'a', 2, 'b', 3, 'c']
+print(a)
+
+#Example on W3schools
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+#My own examples
+#1
+a = [1, 1, 1, 1, 1, 1]
+print(a)
+#2
+a = ['a', 'b', 'c', 'd', 'c', 'd', 'a']
+print(a)
+#3
+a = [111, 11111, 1111111, 111]
+b = ['aaaaaaa', 'aaaaaaaaa', 'aaa', 'aaa']
+
+print(a)
+print(b)
+#4
+ages = [8, 72, 64, 64, 11]
+avengers = ['ironman', 'superman','black widow' 'captain America', 'black widow']
+
+print(ages)
+print(avengers)
+#5
+a = ['Algida', '18', 'sister', 'automation and control', 'KBTU', 'first-year student', 'sister']
+b = ['Aida', '20','eldest sister' 'islamic scholar', 'NMU', 'fourth-year student', 'eldest sister']
+
+print(a)
+print(b)
+
+#Example on W3schools
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#My own examples
+# 1
+a = ['a', 'b', 'c']
+print(len(a))
+#2
+a = [1, 11, 12, 14, 167]
+print(len(a))
+#3
+a = ['1', '2', '11', '22', '44']
+b = [True, False, 'sorry']
+
+print(len(b))
+print(len(a))
+#4
+a = [1, 22, 33, 222, 333, 2222, 3333, 22222, 33333]
+b = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+c = ['discrete structures', 'calculus 2', 'physics 1', 'history of Kazakhstan', 'english', 'PP_2']
+
+print(len(a))
+print(len(b))
+print('I have in this semester'+len(c)+'disciplines')
+#5
+a = ['notebook', 'computer', 'smartphone', 'TV', 'Play Station', 'Kindle']
+print(len(a))
+
+#Example on W3schools
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+#My own examples
+#1
+a = ['abcd', 'efgh', 'ijkl', 'mnop']
+b = [12, 24, 36, 48, 60, 72]
+c = [False, False, False, False]
+#2
+a = ['upper bound', 'lower bound', 'unbounded']
+b = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+c = [True, True, True, True, False] 
+#3
+a = [True, False]
+b = [False, True, True, False]
+c = [1, 0, 0, 0, 1, 1, 1]
+#4
+name = ['Algida', 'John', 'Amir', 'Marie']
+surname = ['Asylkhan', 'Matt', 'Kenzhebayev', 'Curie']
+ages = [18, 24, 12, 66]
+#5
+family_types = ['one-parent family', 'extended family', 'nuclear family']
+fam_members = ['dad', 'mom', 'brother', 'sister', 'grandpa', 'grandma']
+
+#Example on W3schools
+list1 = ["abc", 34, True, 40, "male"]
+
+#My own examples
+#1
+a = [True, 11111, 333, 'dear', 'honey']
+#2
+a = [1, 0, 1, True, False]
+#3
+a = ['aa', 'bb', False, False, 11]
+#4
+a = ['x', 'y', 1, 0, True, False]
+#5
+a = ['print', 'cout', 'printf', False, 12212112]
+
+#Example on W3schools
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
+#My own examples
+#1
+a = list(('a', 'b', 'c', 'd'))
+print(a)
+#2
+a = list((2, 4, 6, 8))
+print(a)
+#3
+physics = list(('mechanics', 'termodinamics', 'electricity', 'magnetism', 'quantum physics'))
+print(physics)
+#4
+math = list(('calculus', 'discrete structures', 'math analysis', 'linear algebra', 'geometry'))
+print(math)
+#5
+specialties = list(('engineer', 'programmist', 'teacher', 'ecologist', 'geologist', 'scientist'))
+print(specialties)
+
+#Example on W3schools
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+#My own examples
+#1
+a = [1, 45, 66, 77]
+print(a[3])
+#2
+a = ['a', 'j', 'l', 'v']
+print(a[0])
+#3
+a = ['11111', '322', 333]
+print(a[1])
+#4
+a = ['juice', 'soda', 'wine']
+print(a[0])
+#5
+a = ['strawberry', 'blueberry', 'raspberry', 'cranberry']
+print(a[2])
+
+#Example on W3schools
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+#my own examples
+#1
+a = [1, 11, 111, 1111, 11111, 111111, 1111111, 111111111]
+print(a[-3])
+#2
+a = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+print(a[-4])
+#3
